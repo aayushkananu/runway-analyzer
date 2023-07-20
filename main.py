c6184a3941f0ff091df2bbda1e210bd5e2c9ca5d
aayushkananu <aayushkab.2003@gmail.com>
2023-07-20 19:50:41 -0400

    Update main.py
    new looks added

--- main.py
+++ main.py
@@ -3,27 +3,37 @@ valentino_collection_name = " VALENTINO HAUTE COUTURE SS23"
 
 ## DEFINE THE TRENDS
 valentino_trends = {
+  #material and texture
   'sheer': 0,
   'plaid':0, 
   'ruffles':0,
   'stockings':0,
   'stripes':0, 
-  'ankle_strap':0, 
-  'open_toe':0, 
+  'leather':0,
+
+  #items 
   'blazer':0, 
   'v_neck':0, 
   'hat':0,
   'costume_jewelry':0, 
   'strapless':0, 
+  'asymmetric':0,
+  'mini_dress':0,
   'button_down':0, 
+  'shorts':0,
   'hot_shorts':0, 
-  'leather':0, 
   'maxi_dress':0,
   'maxi_skirt':0,
   'jeans':0,
   'crossbody_bag':0, 
   'clutch':0, 
+  'gloves':0,
+  'tie':0,
+  'bow':0,
+
   'valentino_logo':0, 
+
+  #colors
   'red':0, 
   'purple':0,
   'black':0, 
@@ -35,10 +45,25 @@ valentino_trends = {
   'navy':0, 
   'green':0, 
   'shimmer':0, 
-  'grey':0 }      
+  'grey':0,
 
-## DEFINE THE OUTFITS
+  #shoes
+  'ankle_straps':0, 
+  'open_toe':0, 
+  'ballet_flats':0,
+  'platform_heels':0,
+  'boots':0,
+  }      
+
+## DEFINE THE LOOKS
 valentino_looks = [
-['purple','sheer','maxi_dress'], # LOOK 45
-['purple', 'black', 'stockings'],
+['purple','sheer','maxi_dress', 'bow'], # LOOK 45
+['purple', 'black', 'stockings', 'gloves', 'leather', 'purple' 'ballet_flats','bow'],
+['platform_heels', 'purple', 'maxi_dress', 'asymmetric'],
+['blazer', 'tie', 'button_down', 'purple', 'boots', 'leather'],
+['purple', 'ruffles','maxi_dress', 'white', 'bow'],
+['yellow', 'purple', 'black','bow', 'blazer'],
+['ruffles', 'yellow', 'pink','ankle_straps'],
+['bow', 'gloves','purple', 'pink', 'red', 'maxi_dress'],
+
 ]
