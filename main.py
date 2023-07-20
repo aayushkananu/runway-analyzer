d890f0ac0a473ffaccdd6c36a66ee7f095167f60
aayushkananu <112506495+aayushkananu@users.noreply.github.com>
2023-07-20 19:04:59 -0400

    created main.py and created a dict

---
+++ main.py
@@ -0,0 +1,36 @@
+#### COLLECTION ####
+valentino_collection_name = " VALENTINO HAUTE COUTURE SS23"
+
+## DEFINE THE TRENDS
+valentino_trends = {
+  'sheer': 0,
+  'plaid':0, 
+  'stripes':0, 
+  'ankle_strap':0, 
+  'open_toe':0, 
+  'blazer':0, 
+  'v_neck':0, 
+  'hat':0,
+  'costume_jewelry':0, 
+  'strapless':0, 
+  'button_down':0, 
+  'hot_shorts':0, 
+  'leather':0, 
+  'maxi':0,
+  'crossbody_bag':0, 
+  'clutch':0, 
+  'cc_logo':0, 
+  'chanel_logo':0, 
+  'red':0, 
+  'purple':0,
+  'black':0, 
+  'blue':0, 
+  'yellow':0, 
+  'pink':0, 
+  'orange':0, 
+  'white':0, 
+  'navy':0, 
+  'green':0, 
+  'shimmer':0, 
+  'grey':0 }      
+
