491ca4d48e9d9b45ba9d6bca98249aeeb593c3f8
suranganaaryal <112665888+suranganaaryal@users.noreply.github.com>
2023-07-24 09:42:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ valentino_trends = {
   'stockings':0,
   'stripes':0, 
   'leather':0,
+  'fur':0,
 
   #items 
   'blazer':0, 
@@ -30,6 +31,13 @@ valentino_trends = {
   'gloves':0,
   'tie':0,
   'bow':0,
+  'maxi':0,
+  'parachute pants':0,
+  'cape':0,
+  'goggles':0,
+  'button-up':0,
+  'fishnet':0,
+  'long coat':0,
 
   'valentino_logo':0, 
 
@@ -53,10 +61,55 @@ valentino_trends = {
   'ballet_flats':0,
   'platform_heels':0,
   'boots':0,
+  'close_toed':0,
   }      
 
 ## DEFINE THE LOOKS
 valentino_looks = [
+['tie','pink','white','black','blazer','button_up','bow', 'ballet_flats' ], #look1
+['black','close_toed','goggles','blazer'],
+['black','polka_dots','stockings' ,'ankle_strap'],
+['pink','black','white','googles','cape','fishnet','polka_dots','stockings','platform_heels'],
+['black','pink','parachute_pant','long_coat'],
+['pink','silk','maxi'],
+['long_coat','brown','white','pink', 'bow', 'fur','ballet_flats'],
+['brown','ruffle','ballet_flats'],
+['brown','shimmer','gloves','ballet_flats'],
+['shimmer','blue','black','button_up','long_coat','boots'],
+['orange','purple','bow','gloves','platform_heels'],
+['ruffle','long_coat','plaform_heels' ,'leggings','orange'],
+['pink','brown','parachute_pants'],
+['ruffle','shimmer','red','pink','ballet_flats'],
+['leggings','green','shimmer','pink','ruffle','gloves','ballet_flats'],
+['ruffle','pink','black','brown','gloves','ballet_flats'],
+['long_coat','leggings','platform_heels','ruffle','brown','green','pink'],
+['long_coat', 'ruffle','polka_dots' 'stockings','green','ballet_flats','black','white'], #look18
+['ruffle','cape', 'stripes','ballet_flats','pink','black','green'],
+['pink', 'long_coat','white','black',],
+['pink','maxi','bow', 'close_toed'],
+['pink','black', 'gloves', 'maxi', 'polka_dots','ruffle','close_toed'],
+['ruffle','gloves','ballet_flats' ,'pink','black','green'],
+['stripes','pink','bow','black','yellow', 'ballet_flats'],
+['cape','pink','shimmer','fur', 'bow', 'ballet_flats'],
+['black', 'gloves', 'ballet_flats'],
+['cape','black','ankle_strap'] , #look27
+['tie','boots','button_up','black','white'],
+['fishnet','ruffle','black'],
+['black','feather','ankle_strap','pink'],
+['black','maxi','bow','white'],
+['long coat','fur','button_up','blue'],
+['blazer','tie','button_up','shimmer','black','white'],
+['striped','blazer,'bow','pink', 'ballet_skirt','black','white'],
+['blazer','ruffle','shimmer','fishnet','stockings','gloves','black','white'],
+['black','gloves'],
+['white','net','ballet_flats'],
+['white','maxi','black','bow'],
+['polka_dots', 'white','maxi','close_toed','ankle_strap','brown'],
+['blazer','boots','tie','button_up','yellow','black','white'],
+['tie','button_up','yellow', 'white','black','polka_dots'],
+['polka dots','long_coat,'bow','tie','button_up','ballet_flats','black,'blue','off_white'],
+['bow','green', 'maxi','platform_heels'],
+['ruffle','maxi','platform_heels','green','black'], #look44
 ['purple','sheer','maxi_dress', 'bow'], # LOOK 45
 ['purple', 'black', 'stockings', 'gloves', 'leather', 'purple' 'ballet_flats','bow'],
 ['platform_heels', 'purple', 'maxi_dress', 'asymmetric'],
